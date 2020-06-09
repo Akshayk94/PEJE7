6f6ceb9f52eac047b934f31db5aaff51ccc88285
Akshay <kulkarniakshay82@gmail.com>
2020-06-09 11:08:29 +0530

    programs

---
+++ FactoryDesignPattern/src/domain/Vehicle.java
@@ -0,0 +1,7 @@
+package domain;
+
+public interface Vehicle //connection (I)
+{
+	void getVehicleType();
+	void getVehiclePrice();
+}
---
+++ FactoryDesignPattern/src/test/Central1.java
@@ -0,0 +1,10 @@
+package test;
+
+//functional class
+public class Central1 
+{
+	static
+	{
+		System.out.println("STATIC BLOCK OF CENTRL1");
+	}
+}
